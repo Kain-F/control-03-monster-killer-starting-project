22ee3ac8a943717fa39a566c235677dbd7457dc5
Kain-F <80208910+Kain-F@users.noreply.github.com>
2024-04-27 17:03:09 +0200

    win condition

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 // consts cant change
-const ATTACK_VALUE = 10;
+const PLAYER_ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 15;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -8,6 +9,17 @@ let currentPlayerHealth = chosenMaxLife;
 attackBtn.addEventListener('click',attackHandler);
 
 function attackHandler() {
-    const damage = dealMonsterDamage(ATTACK_VALUE);
+    
+    const damage = dealMonsterDamage(PLAYER_ATTACK_VALUE);
     currentMonsterHealth -= damage;
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage
+    
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('you won');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0){
+        alert('the monster won');
+    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+        alert('draw')
+    } 
 }
